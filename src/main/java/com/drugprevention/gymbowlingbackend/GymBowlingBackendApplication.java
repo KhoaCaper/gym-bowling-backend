b4dca47cfeb3d691e16118ac6f2a0b491ccf1e1a
KhoaCaper <khoacaper@gmail.com>
2025-08-22 20:43:53 +0700

    Simplify profile configuration and fix application.properties

--- src/main/java/com/drugprevention/gymbowlingbackend/GymBowlingBackendApplication.java
+++ src/main/java/com/drugprevention/gymbowlingbackend/GymBowlingBackendApplication.java
@@ -2,20 +2,14 @@ package com.drugprevention.gymbowlingbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import java.util.Properties;
 
 @SpringBootApplication
 public class GymBowlingBackendApplication {
 
     public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(GymBowlingBackendApplication.class);
-        
-        // Set default profile to render if no profile is specified
-        Properties props = new Properties();
-        props.setProperty("spring.profiles.active", "render");
-        app.setDefaultProperties(props);
-        
-        app.run(args);
+        // Set render profile for production deployment
+        System.setProperty("spring.profiles.active", "render");
+        SpringApplication.run(GymBowlingBackendApplication.class, args);
     }
 
 }
